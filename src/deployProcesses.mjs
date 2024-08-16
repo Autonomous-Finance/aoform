dbeb960d4ec749123428980c83a496b6eab1b201
smowden <smowden@users.noreply.github.com>
2024-08-16 12:57:26 +0300

    add module reset option

--- src/deployProcesses.mjs
+++ src/deployProcesses.mjs
@@ -50,8 +50,30 @@ async function deployProcess(processInfo, state) {
     prerunScript = fs.readFileSync(prerunFilePath, 'utf8');
   }
 
+  let resetModulesCode = '';
+  if (processInfo.resetModules) {
+    resetModulesCode = `
+      INITIAL_MODULES = { ".crypto.mac.hmac", "string", ".crypto.cipher.morus", "debug", ".handlers", ".crypto.padding.zero", ".crypto.digest.sha2_256", ".crypto.digest.md2", ".crypto.util.hex", ".default", ".eval", ".crypto.util.bit", ".utils", ".crypto.util.stream", "_G", "json", ".crypto.cipher.norx", ".base64", ".crypto.cipher.aes256", ".crypto.digest.md4", ".crypto.util.queue", ".stringify", ".handlers-utils", ".crypto.cipher.issac", "utf8", ".crypto.cipher.aes", ".dump", ".process", ".crypto.cipher.mode.cfb", "ao", ".pretty", ".crypto.digest.sha1", "coroutine", ".crypto.cipher.aes128", ".crypto.init", ".crypto.digest.sha2_512", ".crypto.cipher.aes192", ".crypto.kdf.pbkdf2", ".crypto.mac.init", ".crypto.digest.init", "package", "table", ".crypto.cipher.mode.ctr", ".crypto.util.array", "bit32", ".crypto.cipher.mode.ecb", ".crypto.kdf.init", ".assignment", ".crypto.cipher.mode.cbc", ".crypto.digest.blake2b", ".crypto.digest.sha3", ".crypto.digest.md5", ".crypto.cipher.mode.ofb", "io", "os", ".chance", ".crypto.util.init", ".crypto.cipher.init" }
+      
+      local function isInitialModule(value)
+          for _, v in pairs(INITIAL_MODULES) do
+              if v == value then
+                  return true
+              end
+          end
+          return false
+      end
+
+      for k, _ in pairs(package.loaded) do
+          if not isInitialModule(k) then
+              package.loaded[k] = nil
+          end
+      end
+      `;
+  }
+
   // Concatenate the prerun script with the main script
-  const luaCode = `${prerunScript}\n${mainScript}`;
+  const luaCode = `${resetModulesCode}\n${prerunScript}\n${mainScript}`;
 
   if (!process.env.WALLET_JSON) {
     console.error("Missing WALLET_JSON environment variable. Please provide the wallet JSON in the environment variable WALLET_JSON.");
